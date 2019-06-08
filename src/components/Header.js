167f65c1042f4d79c2b5dec91175b28a31fc6889
Brian Diehr <diehbria@amazon.com>
2019-06-08 10:41:17 -0700

    Initial clean up

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,18 @@
 import React from 'react'
 
 import Footer from './Footer'
-import avatar from '../assets/images/avatar.jpg'
 
+const avatarUrl = "https://scontent-sea1-1.cdninstagram.com/vp/a8cd449290c253236ccbb3b3abe8a307/5D93A979/t51.2885-19/s320x320/60270542_1189454784548689_8408543490136342528_n.jpg?_nc_ht=scontent-sea1-1.cdninstagram.com"
 class Header extends React.Component {
     render() {
         return (
             <header id="header">
                 <div className="inner">
-                    <a href="#" className="image avatar"><img src={avatar} alt="" /></a>
-                    <h1><strong>I am Strata</strong>, a super simple<br />
-                    responsive site template freebie<br />
-                    crafted by <a href="http://html5up.net">HTML5 UP</a>.</h1>
+                    <a href="#" className="image avatar"><img src={avatarUrl} alt="" /></a>
+                    <h1><strong>Wanders Wild</strong>
+                      <br/>
+                      A wanderer, her dogs, a cat, plus boyfriend of forever. Eternally seeking mountain peaks and the perfect espresso ↟
+                    </h1>
                 </div>
                 <Footer />
             </header>
